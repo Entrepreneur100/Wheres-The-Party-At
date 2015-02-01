8101a66ecdc577512f4f419ebc38189ca5d6b430
ethanp56 <ethanp56@yahoo.com>
2015-02-01 03:54:11 -0600

    SQLite Partial Implementation

--- routes.py
+++ routes.py
@@ -2,7 +2,10 @@
 from flask import Flask, render_template, request, abort, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 import time
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+
 db = SQLAlchemy(app)
 
 print "WEBPAGE COMPILED!"
@@ -19,11 +22,14 @@ class User(db.Model):
 	latitude = db.Column(db.Float, primary_key=True)
 	timestamp = db.Column(db.Float, primary_key=True)
 
-	def __init__(self, identity=None, longitude=None, latitude=None, timestamp=None):
+	def __init__(self, identity, longitude, latitude, timestamp):
 		self.identity = identity
 		self.longitude = longitude
 		self.latitude = latitude
 		self.timestamp = timestamp
+
+	def __repr__(self):
+		return '<USER %r>' % (self.identity)
 
 @app.route('/')
 def themap():
@@ -38,34 +44,44 @@ def handle_send_location():
 	'timestamp' : time.time()
 	}
 
-
 	# write the data to the database using sqlite
 	user = User(identity = data['identity'], longitude = data['longitude'], latitude = data['latitude'], timestamp = data['timestamp'])
+	print user
+	db.session.add(user)
+	db.session.commit()
 	# print user.identity
 	# print user.longitude
 	# print user.latitude
 	# print user.timestamp
 
+	users = db.session.query(User).all()
+
+	for user in users:
+		print "USERRR!"
+
 	abort(401)
 
 @app.route('/get_locations')
 def handle_get_locations():
-  # read the data from the database using sqlite
+	# read the data from the database using sqlite
 
-  # format the records you received into list of dictionaries
-  data = []
+	# format the records you received into list of dictionaries
+	data = []
 
-  data.append({
-      'longitude': 8.0,
-      'latitude': 0.9,
-    })
+	# users = db.session.query(User).all()
 
-  # return the list of dictionaries as json
-  return jsonify(locations=data)
+	# for user in users:
+	# 	print "USER!"
+
+	data.append({
+		'longitude': 8.0,
+		'latitude': 0.9,
+		})
+
+	# return the list of dictionaries as json
+	return jsonify(locations=data)
 
 
-
-  
 if __name__ == '__main__':
     app.run(debug = True)
   
