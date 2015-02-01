94294b10315db70b041d8e828a3e4eb0346909c0
ethanp56 <ethanp56@yahoo.com>
2015-01-31 21:40:06 -0600

    SCHURR FTW Data sends to Python!!

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, jsonify
 app = Flask(__name__)
 
 
@@ -10,7 +10,36 @@ def home():
 def themap():
 	return render_template('geolocation1.html')
 
+@app.route('/send_location', methods=['POST'])
+def handle_send_location():
+	data = {
+		'id' : request.form['id'],
+		'long' : request.form['long'],
+		'lat' : request.form['lat'],
+		'timestamp' : request.form['timestamp']
+	}
+
+	# write the data to the database using sqlite
+
+	abort(401)
+
+@app.route('/get_location')
+def handle_get_location():
+	# read the data from the database using sqlite
+
+	# format the records you received into list of dictionaries
+	data = []
+
+	data.append({
+			'long': 8.0,
+			'lat': 0.9,
+		})
+
+	# return the list of dictionaries as json
+	return jsonify(locations=data)
+
+
 
 	
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
